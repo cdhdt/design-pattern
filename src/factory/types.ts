8bcbceb58b67cd355bd046dc1eed823118a473d2
cdhdt <cdhoudetot@ik.me>
2026-07-17 17:22:19 +0200

    ajout type

--- src/factory/types.ts
+++ src/factory/types.ts
@@ -1,6 +1,6 @@
 import type { GenerateurDocument } from "../core/GenerateurDocument.js";
 import type { Renderer } from "../core/Renderer.js";
 
-export type TypeDocument = "facture" | "devis" | "avoir";
+export type TypeDocument = "facture" | "devis" | "avoir" | "bon-livraison";
 
 export type ConstructeurGenerateur = new (renderer: Renderer) => GenerateurDocument;
